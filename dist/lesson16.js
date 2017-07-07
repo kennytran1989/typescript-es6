2f0d4e3ddc1185b2754b59bf2d0da840b8afe613
YAHOO\thuy <thuy@yahoo-corp.jp>
2017-07-07 16:47:16 +0700

    push lesson 16

--- dist/lesson16.js
+++ dist/lesson16.js
@@ -1,1 +1,16 @@
+var colorName = ["Red", "Green", "Blue", "Yellow", "Purple"];
+//liet ke value cau mang thong qua index
+for (var index in colorName) {
+    console.log(colorName[index]);
+}
+//liêt ke value truc tiep cau mảng
+for (var _i = 0, colorName_1 = colorName; _i < colorName_1.length; _i++) {
+    var item = colorName_1[_i];
+    console.log(item);
+}
+var name4 = "TEDU";
+for (var _a = 0, name4_1 = name4; _a < name4_1.length; _a++) {
+    var c_1 = name4_1[_a];
+    console.log(c_1);
+}
 //# sourceMappingURL=lesson16.js.map
---
+++ lesson16.ts
@@ -0,0 +1,14 @@
+let colorName= ["Red","Green","Blue","Yellow","Purple"];
+//liet ke value cau mang thong qua index
+for(let index in colorName){
+    console.log(colorName[index]);
+}
+//liêt ke value truc tiep cau mảng
+for(let item of colorName){
+    console.log(item);
+}
+
+let name4="TEDU";
+for(let c of name4){
+    console.log(c);
+}
